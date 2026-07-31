fb05eacd25ff18965b5673b18e1346df7baa856c
jerome4551 <tenerife.go.app@gmail.com>
2026-07-31 08:52:03 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,8 +7,6 @@ self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
-self.addEventListener('message', e => { if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting(); });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(()=>self.clients.claim()));
 });
